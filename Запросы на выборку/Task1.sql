16cf5d6374f0dc3e8fba2aa69e410ebe91c6d07b
RostislavIsakov <48453400+RostislavIsakov@users.noreply.github.com>
2021-02-04 10:45:30 +0300

    Delete Запросы на выборку directory

--- Запросы на выборку/Task1.sql
+++
@@ -1,3 +0,0 @@
-use climblingclubdb;
-
-SELECT FIO, Address, Phone FROM Climbers WHERE Category LIKE 'Мастер спорта' ORDER BY FIO;
--- Запросы на выборку/Task4.sql
+++
@@ -1,5 +0,0 @@
-use climblingclubdb;
-
-SELECT ID_Group, FIO FROM climbers_in_groups
-INNER JOIN climbers ON climbers.ID_Climber = climbers_in_groups.ID_Climber 
-WHERE ID_Group IN (SELECT ID_Group FROM climbing WHERE Date_A BETWEEN "2012.01.01" AND "2013.01.01");
